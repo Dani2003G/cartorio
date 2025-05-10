41a6265823da1780f3ac2d26297c89cd3b04bb0c
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2025-05-10 14:14:50 -0300

    Adicao do metodo de abrir o formulario de cadastro e edicao, junto com os metodos de cadastrar e atualizar

--- src/main/java/br/com/cartorio/mvc/cartorio/cartorio/controller/CartorioController.java
+++ src/main/java/br/com/cartorio/mvc/cartorio/cartorio/controller/CartorioController.java
@@ -1,14 +1,14 @@
 package br.com.cartorio.mvc.cartorio.cartorio.controller;
 
+import br.com.cartorio.mvc.cartorio.cartorio.dto.RequisicaoEdicaoCartorioDTO;
 import br.com.cartorio.mvc.cartorio.cartorio.dto.RequisicaoNovoCartorioDTO;
 import br.com.cartorio.mvc.cartorio.cartorio.service.CartorioService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -18,9 +18,39 @@ public class CartorioController {
 
     private final CartorioService service;
 
-    @GetMapping
-    public ModelAndView formulario(RequisicaoNovoCartorioDTO requisicao) {
-        return new ModelAndView("/cartorio/formulario");
+    @GetMapping("/formularioNovo")
+    public ModelAndView formularioNovo() {
+        ModelAndView mv = new ModelAndView("/cartorio/formularioNovo");
+        mv.addObject("cartorio", new RequisicaoNovoCartorioDTO());
+        return mv;
+    }
+
+    @PostMapping("/novo")
+    public ModelAndView cadastrar(@Valid @ModelAttribute("cartorio") RequisicaoNovoCartorioDTO requisicao, BindingResult result) {
+        ModelAndView mv = new ModelAndView("redirect:/home");
+        if (result.hasErrors()) {
+            mv = new ModelAndView("/cartorio/formularioNovo");
+            mv.addObject("cartorio", requisicao);
+        }
+        return mv;
+    }
+
+    @GetMapping("/formularioEdicao/{id}")
+    public ModelAndView formularioEdicao(RequisicaoEdicaoCartorioDTO requisicao, @PathVariable Long id) {
+        ModelAndView mv = new ModelAndView("/cartorio/formularioEdicao");
+        mv.addObject("cartorio", service.buscarPorId(id));
+        return mv;
+    }
+
+    @PutMapping("/atualizar")
+    public ModelAndView atualizar(@Valid @ModelAttribute("cartorio") RequisicaoEdicaoCartorioDTO requisicao, BindingResult result) {
+        if (result.hasErrors()) {
+            ModelAndView mv = new ModelAndView("/cartorio/formularioEdicao");
+            mv.addObject("cartorio", requisicao);
+            return mv;
+        }
+        service.atualizar(requisicao);
+        return new ModelAndView("redirect:/home");
     }
 
     @DeleteMapping("/{id}")
