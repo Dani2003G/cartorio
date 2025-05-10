4450a8904956b27f4b5398ac4f40317fee482f78
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2025-05-10 14:16:06 -0300

    Adicao do WebConfig para poder enviar o formulario de atualizacao como put

---
+++ src/main/java/br/com/cartorio/mvc/cartorio/cartorio/WebConfig.java
@@ -0,0 +1,14 @@
+package br.com.cartorio.mvc.cartorio.cartorio;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
+}
