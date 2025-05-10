28669df7066441a810f2b64affae59038392a341
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2025-05-10 12:24:31 -0300

    Adicao do metodo de excluir um cartorio

--- src/main/java/br/com/cartorio/mvc/cartorio/cartorio/controller/CartorioController.java
+++ src/main/java/br/com/cartorio/mvc/cartorio/cartorio/controller/CartorioController.java
@@ -1,9 +1,13 @@
 package br.com.cartorio.mvc.cartorio.cartorio.controller;
 
 import br.com.cartorio.mvc.cartorio.cartorio.dto.RequisicaoNovoCartorioDTO;
+import br.com.cartorio.mvc.cartorio.cartorio.service.CartorioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,9 +16,17 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/cartorio")
 public class CartorioController {
 
+    private final CartorioService service;
+
     @GetMapping
     public ModelAndView formulario(RequisicaoNovoCartorioDTO requisicao) {
         return new ModelAndView("/cartorio/formulario");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        service.deletar(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
