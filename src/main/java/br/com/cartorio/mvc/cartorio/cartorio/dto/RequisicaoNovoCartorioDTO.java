386559aabe19169d8c73fc1c2562051a1a15005c
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2025-05-10 14:15:36 -0300

    mudado para classe

--- src/main/java/br/com/cartorio/mvc/cartorio/cartorio/dto/RequisicaoNovoCartorioDTO.java
+++ src/main/java/br/com/cartorio/mvc/cartorio/cartorio/dto/RequisicaoNovoCartorioDTO.java
@@ -3,22 +3,27 @@ package br.com.cartorio.mvc.cartorio.cartorio.dto;
 import br.com.cartorio.mvc.cartorio.cartorio.model.Cartorio;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public record RequisicaoNovoCartorioDTO(
-        @NotBlank(message = "O nome é obrigatório")
-        @Length(min = 3, max = 255, message = "O nome tem que ter entre 3 e 255 caracteres")
-        String nome,
+@Getter
+@Setter
+public class RequisicaoNovoCartorioDTO {
+    @NotBlank(message = "O nome é obrigatório")
+    @Length(min = 3, max = 255, message = "O nome tem que ter entre 3 e 255 caracteres")
+    private String nome;
 
-        @NotBlank(message = "O endereco é obrigatório")
-        @Length(min = 3, max = 255, message = "O endereco tem que ter entre 3 e 255 caracteres")
-        String endereco,
+    @NotBlank(message = "O endereco é obrigatório")
+    @Length(min = 3, max = 255, message = "O endereco tem que ter entre 3 e 255 caracteres")
+    private String endereco;
 
-        @NotEmpty(message = "É necessário informar ao menos uma certidão")
-        List<String> certidoes
-) {
+    @NotEmpty(message = "É necessário informar ao menos uma certidão")
+    private List<String> certidoes = new ArrayList<>();
 
     public Cartorio toEntity() {
         Cartorio cartorio = new Cartorio();
